859e19d7c09eaafee334d7339722ef9658c8954d
olchrkk <98900779+olchrkk@users.noreply.github.com>
2023-04-13 22:28:05 +0300

    second commit

--- Homework/hw_7.py
+++ Homework/hw_7.py
@@ -4,11 +4,13 @@ import string
 "the-stealth-warrior" -> "theStealthWarrior"
 "The_Stealth_Warrior" -> "TheStealthWarrior" '''
 
-def camelCase(text:str)->str:
+
+def camelCase(text: str) -> str:
     on_delete = string.punctuation + ' '
     text = text.title()
     new_text = ''.join(i for i in text if i not in on_delete)
     return new_text
+
 
 print(camelCase("the-stealth-warrior"))
 print(camelCase("The_Stealth_Warrior"))
@@ -19,11 +21,13 @@ print(camelCase("The_Stealth_Warrior"))
 191 -> 202
 2541 -> 2552 '''
 
-def palindrom(num:int)->int:
+
+def palindrom(num: int) -> int:
     num += 1
     while str(num) != str(num)[::-1]:
         num += 1
     return num
+
 
 print(palindrom(188))
 
@@ -31,23 +35,24 @@ print(palindrom(188))
 Список будет заполняться рандомными целыми числами. Рекомендую использовать несколько функций
 (для заполнения списка целыми числами, для подсчета количества) '''
 
-def symbols_counting(func):
-    def wrapper():
-        func()
-        if list_num == set(list_num):
-            print('there are no the same elements')
-        else:
-            count = len(list_num) - len(set(list_num))
-            print(f'same numbers appear {count} times')
-        return wrapper
 
-@symbols_counting
+def symbols_counting(list_num):
+    if list_num == set(list_num):
+        print('Sorry! There are no the same elements')
+    else:
+        count = len(list_num) - len(set(list_num))
+        print(f'The same numbers appear {count} times')
+
+
 def my_list():
     while True:
-        a = int(input('enter a number in the list: '))
+        a = int(input('Enter a number in the list: '))
         list_num.append(a)
+        print(f'Your list is ', list_num)
+        symbols_counting(list_num)
 
 
 list_num = []
 
 my_list()
+
