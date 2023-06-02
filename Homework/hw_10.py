ceb038421a2808838d6f73f18fe5c26a9b2550be
olchrkk <98900779+olchrkk@users.noreply.github.com>
2023-06-02 19:10:06 +0300

    Update hw_10.py

--- Homework/hw_10.py
+++ Homework/hw_10.py
@@ -1,19 +1,21 @@
+from datetime import datetime
 '''Создать класс Human c атрибутами name, surname, birth_year и двумя методами
 full_name(), 
 который будет  возвращать полное имя: Name Surname и get_age(), 
 возвращающий возраст, созданного экземпляра.'''
 
 class Human:
+    current_year = datetime.now().year
     def __init__(self, name, surname, birth_year):
         self.name = name
         self.surname = surname
         self.birth_year = birth_year
 
     def full_name(self):
-        return f'{self.name} {self.surname}'
+        return f'{self.name.capitalize()} {self.surname.capitalize()}'
 
     def get_age(self):
-        return 2023 - self.birth_year
+        return f'{Human.current_year- self.birth_year}'
 
 
 
